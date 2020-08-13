cb6105829d09ceb0c5af2c869985d85bac4dbc85
David Speck <david@saduras.de>
2020-08-13 19:27:14 +0200

    E17: Add textures

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,6 +10,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 /* Check documentation at http://docs.gl */
 
@@ -44,23 +45,27 @@ int main(void)
 
 	GLCall(std::cout << glGetString(GL_VERSION) << std::endl);
 
-	float positions[] = {
-		-0.5f, -0.5f, // 0
-		 0.5f, -0.5f, // 1
-		 0.5f,  0.5f, // 2
-		-0.5f,  0.5f, // 3
-	};
-
-	unsigned int indicies[] = {
-		0, 1, 2,
-		2, 3, 0,
-	};
-
 	{
+		float positions[] = {
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f, // 1
+			 0.5f,  0.5f, 1.0f, 1.0f, // 2
+			-0.5f,  0.5f, 0.0f, 1.0f, // 3
+		};
+
+		unsigned int indicies[] = {
+			0, 1, 2,
+			2, 3, 0,
+		};
+
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
@@ -69,7 +74,11 @@ int main(void)
 		Shader shader("res/shader/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-		
+
+		Texture texture("res/textures/pop-me.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
+
 		va.Unbind();
 		vb.Unbind();
 		ib.Unbind();
